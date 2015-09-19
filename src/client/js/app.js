4823b28cd9aa79a6d8a90908c25e8a5306a4a389
Christopher Reid <c29reid@uwaterloo.ca>
2015-09-19 13:46:59 -0600

    improved image stuff

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,8 +1,10 @@
 var canvas = document.getElementById('cvs');
 var ctx = canvas.getContext("2d");
 
-cursorRadius = 5
-playerColour = '#424';
+cursorRadius = 5;
+playerColour = '#0000FF';
+
+mouseDown = false;
 
 function drawCircle(x, y) {
 	x -= 8;
@@ -12,12 +14,43 @@ function drawCircle(x, y) {
 	ctx.arc(x, y, cursorRadius, 0, 2*Math.PI, false);
 	ctx.fill();
 	ctx.closePath();
+	
 }
 
-function handleDraw() {
+function handleMouseDown() {
 	drawCircle(event.pageX, event.pageY);
+	mouseDown = true;
+}
+
+function handleMouseMove() {
+	if (mouseDown) {
+		drawCircle(event.pageX, event.pageY);
+	}
+}
+
+function endDraw() {
+	mouseDown = false;
+}
+
+function initCanvasWithImage(url) {
+	var img = new Image;
+	img.onload = function() {
+		var scale = 1;
+		canvas.height = scale*this.height;
+		canvas.width = scale*this.width;
+		ctx.drawImage(img, 0, 0, this.width, this.height,
+							0, 0, scale*this.width, scale*this.height);
+	}
+	img.src = url;
+
 }
 
 window.onload = function(){ 
-	canvas.addEventListener("mousedown", handleDraw, false);
+	initCanvasWithImage("https://a248.e.akamai.net/f/1202/1579/4m/i.dailymail.co.uk/i/pix/2014/10/06/1412613364603_wps_17_SANTA_MONICA_CA_AUGUST_04.jpg");	
+	canvas.addEventListener("mousedown", handleMouseDown, false);
+	canvas.addEventListener("mousemove", handleMouseMove, false);
+	canvas.addEventListener("mouseup", endDraw, false);
+	canvas.addEventListener("mouseout", endDraw, false);
+	
+	
 }
