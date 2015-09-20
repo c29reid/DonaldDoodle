ed0a091e652da5322bace4eb2a7c41e16c17a6ba
Christopher Reid <c29reid@uwaterloo.ca>
2015-09-20 06:36:51 -0400

    Changing the port to 80

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Hack the north project
+
+To launch a server just run
+npm install
+npm start
--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/../client'));
-var port = 6655;
+var port = 80;
 
 http.listen( port, function() {
 	console.log('Started server on port :' + port);
